102ae29958dd4abe76c0db23d9faae655ecc9d75
mveludo <marco.gouveia@gmail.com>
2020-04-01 20:37:12 -0300

    Teste do branch develop

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 // Testando alterar o arquivo pelo GITHUB
+// Essa alteração foi realizada no branch develop
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
